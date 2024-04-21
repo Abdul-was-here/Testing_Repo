408aa2d465e7c16543ae55a29191f7fae5997c6a
Akshay <Akshay@Akki>
2024-04-22 11:54:50 +1200

    HelloWorld

--- testo/src/newpackage/NewClass.java
+++ testo/src/newpackage/NewClass.java
@@ -3,7 +3,7 @@ package newpackage;
 
 //testing file.123
 public class NewClass {
-   system.out.println("Hello World");
+   system.out.println("Hello World");//Added
    system.out.println("Hello World");
   
 }
